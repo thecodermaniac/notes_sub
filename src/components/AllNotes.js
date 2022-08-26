5dd887446ae0dd0459b20b9afe9230c93b4644dd
Aritra Mondal <sasuke.aritra.sa@gmail.com>
2022-08-26 11:13:05 +0530

    update function working

--- src/components/AllNotes.js
+++ src/components/AllNotes.js
@@ -1,14 +1,32 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState,useRef } from 'react'
 import NotesCard from './NotesCard'
 import { db } from '../firebase'
 import {
   collection,
   query,
-  onSnapshot,
+  onSnapshot, doc, updateDoc,
 } from 'firebase/firestore'
 
 function AllNotes() {
+  let refr = useRef()
+  const [currid, setcurrid] = useState(null)
   const [notesarr, setnotesarr] = useState([])
+  const [editnotes, seteditnotes] = useState({
+    subject: '',
+    title: '',
+    body: ''
+  })
+  const updateNote = (curnote) => {
+    console.log(curnote.id);
+    setcurrid(curnote.id)
+    seteditnotes({ title: curnote.title, subject: curnote.subject, body: curnote.body })
+    refr.current.click()
+  }
+
+  const handleupdate = async () => {
+    await updateDoc(doc(db, "notes", currid), { title: editnotes.title, body: editnotes.body, subject: editnotes.subject });
+  }
+
   useEffect(() => {
     const q = query(collection(db, "notes"));
     const unsub = onSnapshot(q, (querySnapshot) => {
@@ -24,10 +42,41 @@ function AllNotes() {
   console.log(notesarr);
 
   return (
-    notesarr.map((note) => {
-      return <NotesCard note={note} key={note.id}/>
-    })
+    <>
+      <button ref={refr} type="button" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+      </button>
 
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Update your note</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body">
+              <form>
+                <div class="mb-3">
+                  <label for="exampleFormControlInput1" class="form-label">New Title</label>
+                  <input type="text" class="form-control" placeholder="Generic subject title" onChange={(e) => { seteditnotes({ ...editnotes, title: e.target.value }) }} value={editnotes.title} />
+                </div>
+                <div class="mb-3">
+                  <label for="exampleFormControlTextarea1" class="form-label">Full Notes...</label>
+                  <textarea class="form-control" rows="3" onChange={(e) => { seteditnotes({ ...editnotes, body: e.target.value }) }} value={editnotes.body} />
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="submit" class="btn btn-outline-secondary" onClick={handleupdate}>Update Notes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {notesarr.map((note) => {
+        return <NotesCard note={note} key={note.id} updateNote={updateNote} />
+      })}
+    </>
   )
 }
 
--- src/components/NotesTake.js
+++ src/components/NotesTake.js
@@ -1,16 +1,27 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { db } from '../firebase'
-import { collection, addDoc, where, onSnapshot, query } from 'firebase/firestore'
+import {
+    collection, addDoc, where, onSnapshot, query, doc, updateDoc,
+} from 'firebase/firestore'
 import NotesCard from './NotesCard'
 
+
 function NotesTake({ Subject }) {
+    let refr = useRef()
     localStorage.setItem('subject', Subject)
+    const [currid, setcurrid] = useState(null)
     const notesdb = collection(db, 'notes')
     const [categorynotes, setcatNotes] = useState([])
     const [notes, setnotes] = useState({
         subject: localStorage?.getItem('subject'),
         title: "",
         body: ""
+    })
+
+    const [editnotes, seteditnotes] = useState({
+        subject: '',
+        title: '',
+        body: ''
     })
 
     const q = query(notesdb, where("subject", "==", localStorage?.getItem('subject')))
@@ -21,7 +32,15 @@ function NotesTake({ Subject }) {
         })
         setcatNotes(notearr)
     })
-
+    const updateNote = (curnote) => {
+        console.log(curnote.id);
+        setcurrid(curnote.id)
+        seteditnotes({ title: curnote.title, subject: curnote.subject, body: curnote.body })
+        refr.current.click()
+    }
+    const handleupdate = async () => {
+        await updateDoc(doc(db, "notes", currid), { title: editnotes.title, body: editnotes.body, subject: editnotes.subject });
+    }
 
     const handleclick = async (e) => {
         e.preventDefault()
@@ -32,19 +51,49 @@ function NotesTake({ Subject }) {
             body: notes.body
 
         })
-        setnotes({ subject: Subject, title: "", body: "" })
         console.log(notes);
+        setnotes({title:'',body:''})
     }
     return (
         <>
+            <button ref={refr} type="button" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Launch demo modal
+            </button>
+
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Update your note</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+
+                        <div class="modal-body">
+                            <form>
+                                <div class="mb-3">
+                                    <label for="exampleFormControlInput1" class="form-label">New Title</label>
+                                    <input type="text" class="form-control" placeholder="Generic subject title" onChange={(e) => { seteditnotes({ ...editnotes, title: e.target.value }) }} value={editnotes.title} />
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleFormControlTextarea1" class="form-label">Full Notes...</label>
+                                    <textarea class="form-control" rows="3" onChange={(e) => { seteditnotes({ ...editnotes, body: e.target.value }) }} value={editnotes.body} />
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-outline-secondary" onClick={handleupdate}>Update Notes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <form className='container'>
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Title</label>
-                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Generic subject title" onChange={(e) => { setnotes({ ...notes, title: e.target.value }) }} />
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Generic subject title" onChange={(e) => { setnotes({ ...notes, title: e.target.value }) }} value={notes.title}/>
                 </div>
                 <div class="mb-3">
                     <label for="exampleFormControlTextarea1" class="form-label">Full Notes...</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" onChange={(e) => { setnotes({ ...notes, body: e.target.value }) }} />
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" onChange={(e) => { setnotes({ ...notes, body: e.target.value }) }} value={notes.body}/>
                 </div>
                 <button type="submit" class="btn btn-outline-secondary" onClick={handleclick}>Add Notes</button>
             </form>
@@ -52,7 +101,7 @@ function NotesTake({ Subject }) {
             {/* categorywise show of notes */}
             {
                 categorynotes.map((note) => {
-                    return <NotesCard note={note} key={note.id} />
+                    return <NotesCard note={note} key={note.id} updateNote={updateNote} />
                 })
             }
 
